fb27a20441590895433026f2083e4c8050326be2
sergioadonis <sergio.adonis@hotmail.com>
2019-12-10 16:58:54 +0000

    Generated code by SwaggerHub

--- aspnetcore/src/IO.Swagger/Models/MenuArrayResult.cs
+++ aspnetcore/src/IO.Swagger/Models/MenuArrayResult.cs
@@ -30,7 +30,7 @@ namespace IO.Swagger.Models
         /// Gets or Sets Menus
         /// </summary>
         [DataMember(Name="menus")]
-        public List<Restaurant> Menus { get; set; }
+        public List<Menu> Menus { get; set; }
 
         /// <summary>
         /// Returns the string presentation of the object
